13debc1856751c41ca7e15f994d450f2f644fe72
fanifrancs <fanikufran6@gmail.com>
2023-02-05 02:37:49 +0100

    update

--- roman_num_converter.js
+++ roman_num_converter.js
@@ -1,7 +1,7 @@
-function convertToRoman (num) {
-  const decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+const decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
+function convertToRoman(num) {
   let romanized = '';
 
   for (let i = 0; i < decimalValue.length; i++) {
